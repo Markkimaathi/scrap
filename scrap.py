6ac5caa177364cef929f7384394cf62a414b1097
Markkimaathi <markangelkimathi@gmail.com>
2024-08-15 12:51:48 +0300

    scrap

--- scrap.py
+++ scrap.py
@@ -5,7 +5,7 @@ import random
 
 app = Flask(__name__)
 
-# Define a user-agent list to simulate requests from different browsers
+
 USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/53.0',
